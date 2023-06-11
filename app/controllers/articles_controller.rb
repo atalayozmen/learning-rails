7c9e8254761a965d5726aeba0f39c21222c0bfc5
atalayozmen <atalayozmen21@gmail.com>
2023-06-11 17:47:47 +0200

    edit and delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
   end
@@ -11,6 +12,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    puts "start"
+    print(article_params)
+    puts "end"
     @article = Article.new(article_params)
 
     if @article.save
@@ -20,8 +24,30 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
+
     end
 end
