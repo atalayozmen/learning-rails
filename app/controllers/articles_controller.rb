cab12115f871ee5272d491e1dab72ecc21a6faff
atalayozmen <atalayozmen21@gmail.com>
2023-06-12 20:42:46 +0200

    authentication for actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "siemens", password: "siemens", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
